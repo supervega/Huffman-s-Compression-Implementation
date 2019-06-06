5bc572480105fc902537791c9a5a0c3957430599
ammar ammar <ammarvega@hotmail.com>
2019-06-06 19:16:28 +0300

    Add project files.

---
+++ Huffman/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Huffman
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.BtnEncode = new System.Windows.Forms.Button();
+            this.TXTInput = new System.Windows.Forms.TextBox();
+            this.TXTOutput = new System.Windows.Forms.TextBox();
+            this.BtnClose = new System.Windows.Forms.Button();
+            this.BtnTree = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnEncode
+            // 
+            this.BtnEncode.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnEncode.Location = new System.Drawing.Point(352, 91);
+            this.BtnEncode.Name = "BtnEncode";
+            this.BtnEncode.Size = new System.Drawing.Size(104, 26);
+            this.BtnEncode.TabIndex = 0;
+            this.BtnEncode.Text = "Encode";
+            this.BtnEncode.UseVisualStyleBackColor = true;
+            this.BtnEncode.Click += new System.EventHandler(this.BtnEncode_Click);
+            // 
+            // TXTInput
+            // 
+            this.TXTInput.Location = new System.Drawing.Point(161, 49);
+            this.TXTInput.Multiline = true;
+            this.TXTInput.Name = "TXTInput";
+            this.TXTInput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.TXTInput.Size = new System.Drawing.Size(171, 45);
+            this.TXTInput.TabIndex = 1;
+            // 
+            // TXTOutput
+            // 
+            this.TXTOutput.Location = new System.Drawing.Point(161, 113);
+            this.TXTOutput.Multiline = true;
+            this.TXTOutput.Name = "TXTOutput";
+            this.TXTOutput.ReadOnly = true;
+            this.TXTOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.TXTOutput.Size = new System.Drawing.Size(171, 45);
+            this.TXTOutput.TabIndex = 2;
+            // 
+            // BtnClose
+            // 
+            this.BtnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnClose.Location = new System.Drawing.Point(420, 1);
+            this.BtnClose.Name = "BtnClose";
+            this.BtnClose.Size = new System.Drawing.Size(47, 26);
+            this.BtnClose.TabIndex = 3;
+            this.BtnClose.Text = "X";
+            this.BtnClose.UseVisualStyleBackColor = true;
+            this.BtnClose.Click += new System.EventHandler(this.BtnClose_Click);
+            // 
+            // BtnTree
+            // 
+            this.BtnTree.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnTree.Location = new System.Drawing.Point(352, 132);
+            this.BtnTree.Name = "BtnTree";
+            this.BtnTree.Size = new System.Drawing.Size(104, 26);
+            this.BtnTree.TabIndex = 4;
+            this.BtnTree.Text = "Show Tree";
+            this.BtnTree.UseVisualStyleBackColor = true;
+            this.BtnTree.Click += new System.EventHandler(this.BtnTree_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Input Data :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(14, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Compressed Data :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(12, 1);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(184, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Pwered By Supervega";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(468, 174);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnTree);
+            this.Controls.Add(this.BtnClose);
+            this.Controls.Add(this.TXTOutput);
+            this.Controls.Add(this.TXTInput);
+            this.Controls.Add(this.BtnEncode);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Huffman";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnEncode;
+        private System.Windows.Forms.TextBox TXTInput;
+        private System.Windows.Forms.TextBox TXTOutput;
+        private System.Windows.Forms.Button BtnClose;
+        private System.Windows.Forms.Button BtnTree;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ Huffman/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Huffman
+{
+    public partial class MainForm : Form
+    {
+        CompressData CD;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            CD = new CompressData();
+        }
+
+        private void BtnEncode_Click(object sender, EventArgs e)
+        {
+            if (TXTInput.Text != "")
+                TXTOutput.Text = CD.getStringBitStream(CD.HoffmanCompress(TXTInput.Text), TXTInput.Text);
+            else
+                errorProvider1.SetError(TXTInput, "Please Enter Some Value...");
+        }
+
+        private void BtnTree_Click(object sender, EventArgs e)
+        {
+            DrawHoffman DH = new DrawHoffman();
+            DH.root = CD.Result;
+            DH.Show();
+        }
+    }
+}
